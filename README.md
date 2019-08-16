a9e08094e83170a8b43dee0824b0f9abeec05b82
Tony Corsten <acorste@emory.edu>
2019-08-16 00:56:23 -0400

    updated frequencies expected for sampling frequency

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 This project is intended to provide a high resolution datalogger for the Teensy 3.6. The Analog Devices LTC2348-16/-18 supports fully-differential, pseudo-differential and single-ended bipolar signaling at either 16 or 18-bit resolution (1LSB INL). The existing Linduino library does not directly work as a port for Arduino-compatible systems (as it's intended to be used with the Linduino board, not surprisingly!). This project features:
 
 - Arduino-compatible code for the Teensy 3.6 
-- ~20kHz sampling frequency using all 8 channels
+- ~5kHz sampling frequency using all 8 channels
 - SD card writing (although this will not guarantee fixed 20kHz operation due to allocation cluster lag)
 - UART output @ 921600 baudrate, and a compatible MATLAB script for efficient logging. In the future, there will also be a Python executible.
--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,31 @@
 #include <SPI.h>
 #include <SD.h>
 
-#define DELAY_TIME_TICK 50
+#define DELAY_TIME_TICK 150
 #define CS_PIN 15
-#define MAX_LOOPS 100
-#define END_WRITE_LOOP 1000
+#define MAX_LOOPS 1000000
+#define END_WRITE_LOOP 1
 #define US_TO_S 1000000
 
 // function definitions
 void serial_wait_command(uint8_t);
+void print_single_parameter(String, uint16_t);
 // constant definitions
 unsigned int timeStartTick = 0;
 int timeTotalTick = 0;
 unsigned int timeEndTick = 0;
-unsigned int deltaSlice = 0;
+uint16_t deltaSlice = 0;
 unsigned int total_loops = 0;
 unsigned int grand_total_loops = 0;
 char readBuffer[20];
 bool write_data = true;
 bool first_loop = true;
-
+char output[100];
 void setup()
 {
-  Serial.begin(921600);
-  while (!Serial); // wait until the Serial terminal communicates back
+  Serial.begin(4608000);
+  while (!Serial)
+    ; // wait until the Serial terminal communicates back
   delay(2000);
   pinMode(0, OUTPUT);
   pinMode(1, OUTPUT);
@@ -35,8 +37,9 @@ void setup()
   //analogWriteFrequency(2, 100000);
   //analogWrite(2, 127);
   digitalWriteFast(0, LOW);
-  serial_wait_command((2 * DELAY_TIME_TICK * MAX_LOOPS * END_WRITE_LOOP) / US_TO_S);
-  while (!Serial.available()); // wait for a response from the host PC
+  serial_wait_command((DELAY_TIME_TICK * MAX_LOOPS * END_WRITE_LOOP) / US_TO_S);
+  while (!Serial.available())
+    ; // wait for a response from the host PC
 }
 
 void loop()
@@ -76,13 +79,28 @@ void loop()
     if (first_loop)
     {
       first_loop = false;
-      Serial.print("0 "); 
+      print_single_parameter("t", 0);
+      print_single_parameter("a", 0);
+      print_single_parameter("b", 0);
+      print_single_parameter("c", 0);
+      print_single_parameter("d", 0);
+      print_single_parameter("e", 0);
+      print_single_parameter("f", 0);
+      print_single_parameter("g", 0);
+      print_single_parameter("h", 0);
     }
     else
     {
-      Serial.print(deltaSlice);
-      Serial.print(" ");
-      if (deltaSlice > 100)
+      print_single_parameter("t", deltaSlice);
+      print_single_parameter("a", deltaSlice);
+      print_single_parameter("b", deltaSlice);
+      print_single_parameter("c", deltaSlice);
+      print_single_parameter("d", deltaSlice);
+      print_single_parameter("e", deltaSlice);
+      print_single_parameter("f", deltaSlice);
+      print_single_parameter("g", deltaSlice);
+      print_single_parameter("h", deltaSlice);
+      if (deltaSlice > 220)
       {
         digitalWriteFast(0, HIGH);
         //write_data = false;
@@ -117,4 +135,10 @@ void serial_wait_command(uint8_t wait_time)
 {
   Serial.print("Wait: ");
   Serial.println(wait_time);
+}
+
+void print_single_parameter(String identifier, uint16_t value)
+{
+  Serial.print(identifier);
+  Serial.print(value, BIN);
 }
